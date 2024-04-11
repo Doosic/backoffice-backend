9897e10f1407a668bcd7a6a08feb3e5143a67a08
doosicEE <strou73@naver.com>
2024-04-11 15:53:54 +0900

    add Bean BCryptPasswordEncoder

--- src/main/java/com/sideproject/BackofficeApplication.java
+++ src/main/java/com/sideproject/BackofficeApplication.java
@@ -1,13 +1,20 @@
-package com.sideproject.backoffice;
+package com.sideproject;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @SpringBootApplication
 public class BackofficeApplication {
 
   public static void main(String[] args) {
     SpringApplication.run(BackofficeApplication.class, args);
+  }
+
+  @Bean
+  public BCryptPasswordEncoder passwordEncoder() {
+    return new BCryptPasswordEncoder();
   }
 
 }
