58d620fd86dea9d9118690d2f8c623a6620d27cb
doosicEE <strou73@naver.com>
2024-04-16 14:10:36 +0900

    update createAdmin validation 조건 변경

--- src/main/java/com/sideproject/domain/dto/admin/AdminCreateRequestDto.java
+++ src/main/java/com/sideproject/domain/dto/admin/AdminCreateRequestDto.java
@@ -8,11 +8,11 @@ import lombok.Data;
 public class AdminCreateRequestDto {
 
   @NotNull(message = "Email cannot be null")
-  @Size(min = 2, message = "Email not be less than two characters")
+  @Size(min = 6, message = "Email not be less than two characters")
   private String email;
 
   @NotNull(message = "Name cannot be null")
-  @Size(min = 2, message = "Name not be less than two characters")
+  @Size(min = 3, message = "Name not be less than two characters")
   private String name;
 
   @NotNull(message = "Password cannot be null")
